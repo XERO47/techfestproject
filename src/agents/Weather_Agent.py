a9c06287514dca87c3f42e329041a17634ace502
XERO47 <141753439+XERO47@users.noreply.github.com>
2023-10-02 07:19:48 +0000

    folder structure

---
+++ src/agents/Weather_Agent.py
@@ -0,0 +1,11 @@
+from uagents import Agent,Context
+
+user=Agent(
+    name="user",
+    seed="user secret seed",
+    port=8001
+    endpoint="http://127.0.0.1/8001"
+)
+
+@user.on_interval(period=10)
+async def 
