2eb41d931e5e584177959d3a9cf357da8e76e28f
AYUSH SEN <85610426+AyushSenDazzler@users.noreply.github.com>
2023-10-02 12:02:38 +0000

    code

--- src/frontend/streamlit.py
+++ src/frontend/streamlit.py
@@ -4,18 +4,38 @@ import pandas as pd
 from datetime import datetime, timedelta
 import pytz
 import folium
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="my_app")
 
 api_key = "5b5af7a943581522b1aa5ef1102ef5e9"
 
 # Set up the sidebar
 st.sidebar.header("Settings`version 0`")
-location = st.sidebar.text_input("Location", "London, UK")
+location = st.sidebar.text_input("Location", "")
 unit = st.sidebar.selectbox("Unit", ["Celsius", "Fahrenheit"])
 forecast = st.sidebar.checkbox("Show Forecast")
 
+# Row A
+st.markdown('### Metrics')
+col1, col2, col3 = st.columns(3)
+col1.metric("Max", "70 °F", "1.2 °F")
+col2.metric("Min", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
+
+# Get the latitude and longitude of the location using the Nominatim API
+if location:
+    location = geolocator.geocode(location)
+    lat = location.latitude
+    lng = location.longitude
+else:
+    # Default to London, UK if no location is provided
+    lat, lng = 51.5074, -0.1278
+
+
 # Add a global map
 st.title("Global Temperature Map")
-m = folium.Map()
+m = folium.Map(location=[lat, lng], zoom_start=10)
 folium.TileLayer(
     tiles="https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid={api_key}",
     attr="OpenWeatherMap",
@@ -25,18 +45,18 @@ folium.TileLayer(
 st.write(m._repr_html_(), unsafe_allow_html=True)
 
 # Add a chart
-st.title("Temperature Over Time")
-start_time = datetime.now(pytz.utc) - timedelta(hours=24)
-end_time = datetime.now(pytz.utc)
-url = f"https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={data['coord']['lat']}&lon={data['coord']['lon']}&start={int(start_time.timestamp())}&end={int(end_time.timestamp())}&appid={api_key}&units=metric"
-response = requests.get(url)
-data = response.json()
-df = pd.DataFrame(data['hourly'])
-df['dt'] = pd.to_datetime(df['dt'], unit='s')
-df.set_index('dt', inplace=True)
-if unit == "Fahrenheit":
-    df = df.apply(lambda x: (x * 9/5) + 32)
-st.line_chart(df['temp'])
+# st.title("Temperature Over Time")
+# start_time = datetime.now(pytz.utc) - timedelta(hours=24)
+# end_time = datetime.now(pytz.utc)
+# url = f"https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={data['coord']['lat']}&lon={data['coord']['lon']}&start={int(start_time.timestamp())}&end={int(end_time.timestamp())}&appid={api_key}&units=metric"
+# response = requests.get(url)
+# data = response.json()
+# df = pd.DataFrame(data['hourly'])
+# df['dt'] = pd.to_datetime(df['dt'], unit='s')
+# df.set_index('dt', inplace=True)
+# if unit == "Fahrenheit":
+#     df = df.apply(lambda x: (x * 9/5) + 32)
+# st.line_chart(df['temp'])
 
 # Add CSS styling
 st.markdown("""
@@ -48,22 +68,22 @@ body {
 """, unsafe_allow_html=True)
 
 # Set up the reminder
-if st.sidebar.button("Set Reminder"):
-    min_temp = st.sidebar.number_input("Minimum Temperature")
-    max_temp = st.sidebar.number_input("Maximum Temperature")
-    reminder = st.sidebar.text_input("Reminder Message")
-    if min_temp and max_temp and reminder:
-        if temp_min < min_temp or temp_max > max_temp:
-            st.warning(reminder)
+# if st.sidebar.button("Set Reminder"):
+#     min_temp = st.sidebar.number_input("Minimum Temperature")
+#     max_temp = st.sidebar.number_input("Maximum Temperature")
+#     reminder = st.sidebar.text_input("Reminder Message")
+#     if min_temp and max_temp and reminder:
+#         if temp_min < min_temp or temp_max > max_temp:
+#             st.warning(reminder)
 
 # Show forecast
-if forecast:
-    st.title("Weather Forecast")
-    url = f"https://api.openweathermap.org/data/2.5/forecast?q={location}&appid={api_key}&units=metric"
-    response = requests.get(url)
-    data = response.json()
-    for forecast in data['list']:
-        dt = datetime.fromtimestamp(forecast['dt'])
-        if dt.hour == 12:
-            st.write(
-                f"{dt.strftime('%A %B %d %Y')}: {forecast['weather'][0]['description']}, {forecast['main']['temp']}°{unit[0]}")
+# if forecast:
+#     st.title("Weather Forecast")
+#     url = f"https://api.openweathermap.org/data/2.5/forecast?q={location}&appid={api_key}&units=metric"
+#     response = requests.get(url)
+#     data = response.json()
+#     for forecast in data['list']:
+#         dt = datetime.fromtimestamp(forecast['dt'])
+#         if dt.hour == 12:
+#             st.write(
+#                 f"{dt.strftime('%A %B %d %Y')}: {forecast['weather'][0]['description']}, {forecast['main']['temp']}°{unit[0]}")
