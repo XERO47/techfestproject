2a2910f33fc30e363b5a56d1057250e2ed0797b0
AYUSH SEN <85610426+AyushSenDazzler@users.noreply.github.com>
2023-10-04 13:33:46 +0000

    updated code

--- src/utils/fetch.py
+++ src/utils/fetch.py
@@ -4,21 +4,6 @@ import os
 load_dotenv()
 api_key=os.getenv('Weather_API_key')
 
-<<<<<<< HEAD
-def fetch_realtime_weather_data(lat,lon):
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
-    headers = {"accept": "application/json"}
-    response = requests.get(url, headers=headers)
-    return(response.json())
-
-def fetch_weather_forecast(location):
-    url = f"https://api.tomorrow.io/v4/weather/forecast?location={location}&apikey={api_key}"
-    headers = {"accept": "application/json"}
-    response = requests.get(url, headers=headers)
-    return(response.text)
-
-print(fetch_realtime_weather_data(18.51,73.85))
-=======
 def fetch_realtime_weather_data(location):
     url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={location}"
     response = requests.get(url)
@@ -29,4 +14,3 @@ def fetch_weather_forecast(location):
     response = requests.get(url)
     return response.text
 
->>>>>>> ccfd40d (updated code)
