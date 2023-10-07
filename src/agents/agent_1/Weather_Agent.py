a784e73ad51018014962038d09715467c9b223d0
XERO47 <141753439+XERO47@users.noreply.github.com>
2023-10-07 12:01:14 +0000

    changes

--- src/agents/agent_1/Weather_Agent.py
+++ src/agents/agent_1/Weather_Agent.py
@@ -19,10 +19,13 @@ fund_agent_if_low(user.wallet.address())
 # user.storage.set('status',[True])[0]
 @user.on_interval(period=2)
 async def call_agent_api(ctx: Context):
-    for i in range(len(ctx.storage.get('lat',))):
-        lat=ctx.storage.get('lat')[i]
-        lon=ctx.storage.get('lon')[i]     
-        await ctx.send(Weather_agent_address,Location_share(lat=f'{lat}',lon=f"{lon}"))
+    if(ctx.storage.get('lat',)==None):
+        pass
+    else:
+       for i in range(len(ctx.storage.get('lat',))):
+           lat=ctx.storage.get('lat')[i]
+           lon=ctx.storage.get('lon')[i]     
+           await ctx.send(Weather_agent_address,Location_share(lat=f'{lat}',lon=f"{lon}"))
 
     
 @user.on_message(model=Temperature_reply)
